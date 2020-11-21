658b4681efe7da302ad835a0ab156ec3b522a7a6
Arthur Santana <artsantana1457@gmail.com>
2020-11-21 19:28:47 -0300

    Added register to combine reducers

--- src/store/states/index.ts
+++ src/store/states/index.ts
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 import { reducer as auth } from '../states/auth'
+import { reducer as register } from '../states/register'
 
-const appReducer = combineReducers({ auth })
+const appReducer = combineReducers({ auth, register })
 
 export default appReducer
